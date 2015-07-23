c1cc70dcb4818e528a145532ddc78ba5259aeac0
mikemsrk <min2033@gmail.com>
2015-07-22 18:11:15 -0700

    Styling front page and new thread button move.

--- pub/app/components/front/front-threaditem.js
+++ pub/app/components/front/front-threaditem.js
@@ -26,14 +26,16 @@ var formatBody = function(str){
 // Front page thread list
 var ThreadItem = React.createClass({
 
-  upVote: function(){
+  upVote: function(e){
+    e.preventDefault();
     this.props.onUpVote(this.props.item.thread_id);
     React.findDOMNode(this.refs.up).className = '';
     React.findDOMNode(this.refs.down).className="glyphicon glyphicon-chevron-down"
     this.props.item.rating++;
   },
 
-  downVote: function(){
+  downVote: function(e){
+    e.preventDefault();
     this.props.onDownVote(this.props.item.thread_id);
     React.findDOMNode(this.refs.down).className = '';
     React.findDOMNode(this.refs.up).className = "glyphicon glyphicon-chevron-up";
@@ -47,27 +49,23 @@ var ThreadItem = React.createClass({
     var body = formatBody(this.props.item.body);
 
     return (
-      <tr>
-        <td>
-          <a href="#" ref="down" className="glyphicon glyphicon-chevron-down" aria-hidden="true" onClick={this.downVote}></a> {this.props.item.rating} <a href="#" ref="up" className="glyphicon glyphicon-chevron-up" aria-hidden="true" onClick={this.upVote}></a></td>
-        <td><a href={"#/thread/"+this.props.item.thread_id}>{this.props.item.title}</a></td>
-        <td>{body}</td>
-        <td><a href={"#/user/"+this.props.item.user_id}> {this.props.item.user_name} </a> </td>
+      <div className="card frontPage">
+        <img src="assets/thread_icon.png"></img>
 
-        <td>
-          <FormattedDate
-            value={created}
-            day="numeric"
-            month="long"
-            year="numeric" />
-        </td>
+        <div className="title">
+          <h2><a href={"#/thread/"+this.props.item.thread_id}>{this.props.item.title}</a></h2>
+          <p>{body}</p>
+        </div>
 
-        <td>
-        <FormattedRelative 
-            value= {updated} />
-        </td>
-
-      </tr>
+        <div className="info">
+          <p>created <FormattedRelative value={created} /> by <a href={"#/user/"+this.props.item.user_id}> {this.props.item.user_name} </a> <span className="updateInfo"> <FormattedRelative value= {updated} /> updated_ </span></p>
+        </div>
+        
+        <div className="votes">
+          <a href="#" ref="down" className="glyphicon glyphicon-chevron-down" aria-hidden="true" onClick={this.downVote}></a> {this.props.item.rating} <a href="#" ref="up" className="glyphicon glyphicon-chevron-up" aria-hidden="true" onClick={this.upVote}></a>
+        </div>
+        
+      </div>
     );
   }
 });
